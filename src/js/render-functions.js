f5cd277e9294241bef5cf25fe8db646c25492cb9
Vitalii Hromyk <hromykk@gmail.com>
2026-09-24 22:25:59 +0300

    add markup

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -5,29 +5,55 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const galleryRef = document.querySelector('.gallery');
 const loaderRef = document.querySelector('.loader');
 
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  fadeSpeed: 300,
+  overlayOpacity: 0.9,
+  closeOnOverlayClick: true,
+  captions: true,
+});
+
 function createGallery(images) {
-  const markup = images.map(
-    image => `<li class="gallery-item">
-                  <a class="gallery-link" href=${image.largeImageURL}>
-                     <img class="gallery-image"
-                          src="${image.webformatURL}"
-                          alt="${image.tags}"
-                          data-source="${image.largeImageURL}"
-                          data-tags="${image.tags}" />
-                  </a>
-                </li>`
-  );
+  const markup = images
+    .map(
+      image => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${image.largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${image.webformatURL}"
+              alt="${image.tags}"
+              data-source="${image.largeImageURL}"
+              data-tags="${image.tags}"
+            />
+          </a>
+          <ul class="info-block">
+            <li>
+              <span>Likes</span>
+              <span>${image.likes}</span>
+            </li>
+            <li>
+              <span>Views</span>
+              <span>${image.views}</span>
+            </li>
+            <li>
+              <span>Comments</span>
+              <span>${image.comments}</span>
+            </li>
+            <li>
+              <span>Downloads</span>
+              <span>${image.downloads}</span>
+            </li>
+          </ul>
+        </li>
+      `
+    )
+    .join('');
 
-  galleryRef.insertAdjacentHTML('beforeend', markup.join(''));
+  galleryRef.insertAdjacentHTML('beforeend', markup);
 
-  new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    fadeSpeed: 300,
-    overlayOpacity: 0.9,
-    closeOnOverlayClick: true,
-    captions: true,
-  }).refresh();
+  lightBox.refresh();
 }
 
 function clearGallery() {
